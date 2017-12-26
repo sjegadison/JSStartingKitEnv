3442421fc5a42fde28c132f697dd51bdcf9bbb4d
unknown <sivaguru.jegadison@gmail.com>
2017-12-26 20:07:19 +0530

    First Module to Github Repository

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// this file isn't transpiled , so must use commonJS and ES5
+
+//Register babel to transpile before our tests run
+require('babel-register')();
+
+//Disable webpack features that mocha doesn't understand
+require.extensions['css'] = function() {};
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+import numeral from 'numeral';
+
+/* eslint-disable no-console */
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+// debugger;
+console.log(`I will pay ${courseValue} for this awesome course!`);
+
